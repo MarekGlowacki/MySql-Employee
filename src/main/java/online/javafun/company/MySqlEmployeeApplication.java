dbae9d20245eb90c31ebada575e29d0e64ecced5
User <user@its.me>
2023-01-07 09:18:21 +0100

    MySQL project init

---
+++ src/main/java/online/javafun/company/MySqlEmployeeApplication.java
@@ -0,0 +1,13 @@
+package online.javafun.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySqlEmployeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySqlEmployeeApplication.class, args);
+    }
+
+}
